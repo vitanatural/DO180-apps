75018266ffee2e3ba65242d28c71fd6783c1e930
Student User <student@workstation.lab.example.com>
2021-09-24 15:58:10 -0400

    Changed old sha sha256:7601775d4fdac9ce55e24bace5b8ca16d9544360533e41a53eba9c4c9b4bfe1a

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "A change is a coming!\n";
+print "OLD sha sha256:7601775d4fdac9ce55e24bace5b8ca16d9544360533e41a53eba9c4c9b4bfe1a"
 ?>
